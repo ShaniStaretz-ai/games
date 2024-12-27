eb80a22cbd941204c87e9008f10723efbd01a472
Shani Staretz <shakimi88@gmail.com>
2024-12-27 13:52:58 +0200

    added players and switch player

--- memory.py
+++ memory.py
@@ -9,10 +9,11 @@ def init_game(cards_labels, rows_dimension) -> dict[str, any]:
     # if not is_rematch:
     return {
         'board': init_board(cards, rows_dimension),
-        'turn': 'X',
+        'turn': '1',
         'rows_dimension': rows_dimension,
         'cols_dimension': cols_dimension,
         'players': {},
+        'computer_mode':False
 
     }
 
@@ -92,24 +93,25 @@ def get_players(game: dict[str, any], is_rematch=False) -> None:
     """
     if not is_rematch:
         players = {}
-        icons = game['icons'].copy()
+
         is_computer = get_valid_boolean_response("do you want to play against the computer (y/n)?", ['y', 'n'], 'y')
 
         if not is_computer:
             count = 1
-            while count < 3:
+            num_of_players = int(input("how many players are you?"))
+            while count <= num_of_players:
                 name = get_valid_player_name(f"player #{count}, please enter your name:")
-                icon = get_player_icon(icons)
-                print(f"you will play the {icon} symbol in this game")
-                players[f'{icon}'] = name
+
+                print(f"you will play the {count} in turn")
+                players[f'{count}'] = {
+                    'name': name,
+                    "score": 0
+                }
                 count += 1
         else:
             name = get_valid_player_name("please enter your name:")
-            icon = get_player_icon(icons)
-            print(f"you will play the '{icon}' symbol in this game")
-            players[f'{icon}'] = name
-            icon = get_player_icon(icons)
-            players[f'{icon}'] = 'computer'
+            players['1'] = {"name": name, "score": 0}
+            players['2'] = {"name": 'computer', "score": 0}
         game['players'] = players
         game['computer_mode'] = is_computer
 
@@ -156,19 +158,17 @@ def draw_board(game: dict[str, any]) -> None:
     print()
     board = game['board']
 
-    # for i in range(1, game['cols_dimension'] + 1):
-    #     print(i, end=' ')
     col_count = 1
     for location, card in board.items():
         if location[1] == 0:
             print(col_count, end='')
             col_count += 1
-        print(' _' if not card['is_flipped'] and not card['is_matched'] else card.icon, end="")
+        print(f" {'_' if not card['is_flipped'] and not card['is_matched'] else card['icon']}", end="")
         if location[1] == game['cols_dimension'] - 1:
             print()
 
 
-def input_square(game: dict[str, any]) -> list[int]:
+def input_card_location(game: dict[str, any]) -> tuple[int, int]:
     """
     get from the user cell location and validate its values:
     check the location limit within the game
@@ -176,26 +176,28 @@ def input_square(game: dict[str, any]) -> list[int]:
     :param game: dictionary of the played game
     :return: list of 2 values, of the next played cell in the game
     """
-    if game['computer_mode'] and game['players'][game['turn']] == 'computer':
-        return get_random_location(game)
+    # if game['computer_mode'] and game['players'][game['turn']] == '2':
+    #     return get_random_location(game)
     while True:
         location: str = input(
-            f"enter row number,column number for {game['players'][game['turn']]}({game['turn']}) separated by ',':")
+            f"player #{game['turn']}, enter row number,column number for  separated by ',':")
         location_list = location.split(',')
         if len(location_list) < 2 or not all([x != '' for x in location_list]) or not all(
                 [x.isdigit() for x in location_list]):
             print("try again,invalid input")
             continue
         location_list = [int(x) - 1 for x in location_list]
-        if not 0 <= location_list[0] < len(game['board']) or not 0 <= location_list[1] < len(game['board']):
+        if not 0 <= location_list[0] < game['rows_dimension'] or not 0 <= location_list[1] < game['rows_dimension']:
             print("try again,out of range")
             continue
-        if game['board'][location_list[0]][location_list[1]] != '_':  # o(1)
-            print("occupied,try again")
+        card = game['board'][tuple(location_list)]
+        if card['is_flipped']:  # o(1)
+            print("already flipped,try again")
             continue
         break
-
-    return location_list
+    print(tuple(location_list))
+    print(len(tuple(location_list)))
+    return tuple(location_list)
 
 
 def get_random_location(game: dict[str, any]):
@@ -206,65 +208,25 @@ def get_random_location(game: dict[str, any]):
     return choice(list(game['moves']))
 
 
-def set_square(game: dict[str, any], location: list[int]) -> None:
+def flip_card(game: dict[str, any], location) -> None:
     """
     update the board with the received list and the current symbol turn
     :param game:dictionary of the played game
     :param location: a list of 2 integers
     """
-    game['board'][location[0]][location[1]] = game['turn']
-    game['moves'].discard(tuple(location))
+    game['board'][location]['is_flipped'] = not game['board'][location]['is_flipped']
 
 
-def check_win(game: dict[str, any]) -> bool:
+def check_match(game, location1: tuple[int, int], location2: tuple[int, int]) -> bool:
     """
     check the current board for win
+    :param location2: location of the 2end card on the board
+    :param location1: location of the 1st card on the board
     :param game: dictionary of the  played game
     :return:True if there is a win, else return False
     """
-    return check_win_rows(game) or check_win_columns(game) or check_win_diagonals(game)
 
-
-def check_win_rows(game: dict[str, any]) -> bool:
-    """
-    :param game: dictionary of the played game
-    :return:True if there is a win in one of the boards' rows, else return False
-    """
-    board = game['board']
-    player = game['turn']
-    is_win = False
-    for row in board:
-        if all(cell == player for cell in row):
-            is_win = True
-    return is_win
-
-
-def check_win_columns(game: dict[str, any]) -> bool:
-    """
-       :param game: dictionary of the played game
-       :return:True if there is a win in one of the boards' columns, else return False
-    """
-    board = game['board']
-    player = game['turn']
-
-    for col_index in range(len(board)):
-        if all([board[index][col_index] == player for index in range(len(board))]):
-            return True
-
-    return False
-
-
-def check_win_diagonals(game: dict[str, any]) -> bool:
-    """
-       :param game: dictionary of the played game
-       :return:True if there is a win in one of the boards' diagonals, else return False
-    """
-    board = game['board']
-    player = game['turn']
-
-    d1 = all([board[i][len(board) - 1 - i] == player for i in range(len(board))])
-    d2 = all([board[i][i] == player for i in range(len(board))])
-    return d1 or d2
+    return game['board'][location1]['id'] == game['board'][location2]['id']
 
 
 def check_tie(game: dict[str, any]) -> bool:
@@ -275,15 +237,22 @@ def check_tie(game: dict[str, any]) -> bool:
     return len(game['moves']) == 0
 
 
-def switch_player(game: dict[str, any]) -> None:
+def switch_player( game: dict[str, any]) -> None:
     """
     switch the current player and update the game
     :param game:dictionary of the played game
     """
-    game['turn'] = 'O' if game['turn'] == 'X' else 'X'
+    if game['computer_mode']:
+        game['turn'] = '1' if game['turn'] == '2' else '2'
+    else:
+        current_player=int(game['turn'])
+        if int(current_player)<len(game['players'].keys()):
+            game['turn']=str(current_player+1)
+        else:
+            game['turn']='1'
 
 
-def play_tic_tac_toe() -> None:
+def play_memory_game() -> None:
     """
     manage the tic-tac-toe game flow
     """
@@ -292,31 +261,53 @@ def play_tic_tac_toe() -> None:
     my_game: dict[str, any] = {}
 
     while new_game or is_rematch:
-        print("Let play Tic - Tac - Toe!!")
-        my_game = init_game(3, my_game, is_rematch)
-        get_players(my_game, is_rematch)
-        print(f"the {my_game['turn']} starts first move")
-        draw_board(my_game)
-        while True:
-            location = input_square(my_game)
-            set_square(my_game, location)
-            draw_board(my_game)
-            if check_win(my_game):
-                print(f"the winner is:{my_game['players'][my_game['turn']]}!")
-                break
-            if check_tie(my_game):
-                print("game over")
-                break
-            switch_player(my_game)
-        is_rematch = get_valid_boolean_response("do you want a rematch (y/n) ?", ['y', 'n'], 'y')
+        print("Let play Memory game!!")
+        card_labels = ('A', 'B', 'C', 'D', 'E', 'F')
 
-        if is_rematch:
-            new_game = False
-            continue
-        else:
-            new_game = get_valid_boolean_response("do you want to play a new game (y/n)?", ['y', 'n'], 'y')
-    else:
-        print("goodbye!")
+        my_game = init_game(card_labels, 4)
+        get_players(my_game, is_rematch)
+        while True:
+            draw_board(my_game)
+            print(my_game)
+            location1 = input_card_location(my_game)
+            flip_card(my_game, location1)
+            draw_board(my_game)
+            location2 = input_card_location(my_game)
+            flip_card(my_game, location2)
+            draw_board(my_game)
+            if check_match(my_game, location1, location2):
+                print("MATCH")
+                set_match(my_game, location1, location2)
+            else:
+                print("NO MATCH")
+                flip_card(my_game, location1)
+                flip_card(my_game, location2)
+                switch_player(my_game)
+
+        # my_game = init_game(3, my_game, is_rematch)
+    #     get_players(my_game, is_rematch)
+    #     print(f"the {my_game['turn']} starts first move")
+    #     draw_board(my_game)
+    #     while True:
+    #         location = input_card_location(my_game)
+    #         flip_card(my_game, location)
+    #         draw_board(my_game)
+    #         if check_match(my_game):
+    #             print(f"the winner is:{my_game['players'][my_game['turn']]}!")
+    #             break
+    #         if check_tie(my_game):
+    #             print("game over")
+    #             break
+    #         switch_player(my_game)
+    #     is_rematch = get_valid_boolean_response("do you want a rematch (y/n) ?", ['y', 'n'], 'y')
+    #
+    #     if is_rematch:
+    #         new_game = False
+    #         continue
+    #     else:
+    #         new_game = get_valid_boolean_response("do you want to play a new game (y/n)?", ['y', 'n'], 'y')
+    # else:
+    #     print("goodbye!")
 
 
 def init_board(cards, rows_num):
@@ -329,11 +320,11 @@ def init_board(cards, rows_num):
     return board
 
 
-if __name__ == "__main__":
-    card_labels = ('A', 'B', 'C', 'D', 'E', 'F')
-    # cards = init_cards(card_labels)
-    # my_board = init_board(len(card_labels),cards)
+def set_match(my_game, location1, location2):
+    my_game['board'][location1]['is_matched'] = True
+    my_game['board'][location2]['is_matched'] = True
+    my_game['players'][my_game['turn']]['score']+=1
 
-    my_game = init_game(card_labels, 4)
-    draw_board(my_game)
-    print(my_game)
+
+if __name__ == "__main__":
+    play_memory_game()
